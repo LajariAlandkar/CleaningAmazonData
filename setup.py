8b28c708e453a2242ee7fb2909376380a33add4d
LajariAlandkar <49995045+LajariAlandkar@users.noreply.github.com>
2020-07-19 19:57:16 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 07:37:35 2020
+
+@author: Lajari
+"""
+
+import setuptools
+
+setuptools.setup(name = 'CleaningAmazonData',
+      version = '0.0.1',
+      description = 'Package to clean Amazon scraped data',
+      author = 'Lajari Alandkar',
+      packages = setuptools.find_packages(),
+      url = 'https://github.com/LajariAlandkar/CleaningAmazonData.git',
+      install_requires =['pandas>=1.0.3', 
+                         'nltk>=3.4.5', 
+                         're>=2.2.1', 
+                         'sklearn>=0.22.1']
+      
+      )
+
+
+
